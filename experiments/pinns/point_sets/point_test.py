a6f9e73f1bdf14d8ddc3811a224f85fc87ffe43f
Alexey Voronin <22531364+Alexey-Voronin@users.noreply.github.com>
2025-04-28 15:04:00 -0400

    more sovlers

--- experiments/pinns/point_sets/point_test.py
+++ experiments/pinns/point_sets/point_test.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
@@ -6,15 +7,21 @@ import matplotlib.pyplot as plt
 from run_pinns import *
 
 
+# Argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument('--training', choices=['sgd', 'smw', 'gn'], required=True)
+args = parser.parse_args()
+training_mode = args.training
+
+
+
 os.environ['JAX_PLATFORM_NAME'] = 'gpu'
 jax.config.update("jax_enable_x64", True)
 jax.config.update('jax_default_matmul_precision', 'highest')
 
 
-
-
 # Settings
-base_path = "./sgd"
+base_path = f"./{training_mode}"
 os.makedirs(base_path, exist_ok=True)
 
 # Sweep parameters
@@ -22,10 +29,24 @@ interior_scales = [1, 2, 4, 12]  # scale num_interior
 boundary_scales = [1, 2, 4]     # scale num_boundary
 NM = [(1,1), (2,2)]          # sin(pi x) sin(pi y) frequencies
 dims = 256                  # model width
-num_iterations = 50_000     # total training steps
 batch_size = -1             # full batch (-1 means full batch)
-learning_rate = 1e-3        # small LR favors grokking
-momentum = 0.0              # SGD without momentum
+
+# Global training settings
+if training_mode == 'sgd':
+    lr = 1e-3
+    lm_info = None
+    num_iterations = 50_000
+elif training_mode == 'smw':
+    lr = 2e-2
+    lm_info = PrecondData(method='smw', epoch=0, lamb=1e-1)
+    num_iterations = 10_000
+elif training_mode == 'gn':
+    lr = 5e-3
+    lm_info = PrecondData(method='gn-exact', epoch=0, thresh=1e-3)
+    num_iterations = 5_000
+else:
+    raise ValueError(f"Unknown training mode {training_mode}")
+
 
 # Functions
 def get_forcing_fxn(n, m):
@@ -62,7 +83,7 @@ for int_scale in interior_scales:
             # Model and optimizer
             model = MLP_1D(1, dims)
             dummy_input = jnp.ones((1, 2))
-            state = create_train_state(model, learning_rate=learning_rate, momentum=momentum, optimizer='sgd', shape=dummy_input)
+            state = create_train_state(model, learning_rate=lr, momentum=o, optimizer='sgd', shape=dummy_input)
 
             # Train
             state, metrics = train_model(
@@ -71,7 +92,7 @@ for int_scale in interior_scales:
                 forcing_function=get_forcing_fxn(n, m),
                 solution_function=get_solution_fxn(n, m),
                 num_iterations=num_iterations,
-                lm_schedule=None,
+                lm_schedule=lm_info,
                 batch_size=batch_size,
                 metrics_file=metrics_file,
             )
